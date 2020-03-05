6215391f7090cab868276546b54b0a9d83641787
courage173 <courageosemwengie@gmail.com>
2020-03-05 15:28:34 +0100

    initial commit

---
+++ VendorMis/Models/DocumentUpload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendorMis.Models
+{
+    public class DocumentUpload
+    {
+        public int DocumentUploadId { get; set; }
+        public int VendorId { get; set; }
+        public String Document { get; set; }
+        public String Creator { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
