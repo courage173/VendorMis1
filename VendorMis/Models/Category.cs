e9557730d9fd7ca3d3b381d382405a518875956f
courage173 <courageosemwengie@gmail.com>
2020-03-05 14:46:09 +0100

    created Category Model

---
+++ VendorMis/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendorMIS.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Creator { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
