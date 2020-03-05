9570ca9dea2e810262fbf6a8a49cb1406b3664ab
Benjamin Daziba Inemugha <bennyblasco@gmail.com>
2020-03-05 15:53:43 +0100

    Added Critical Document Model

--- VendorMis/Models/Category.cs
+++ VendorMis/Models/Category.cs
@@ -8,9 +8,9 @@ namespace VendorMIS.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        public String Name { get; set; }
-        public String Description { get; set; }
-        public String Creator { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Creator { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
---
+++ VendorMis/Models/CriticalDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendorMis.Models
+{
+    public class CriticalDocument
+    {
+        public int CriticalDocumentId { get; set; }
+        public int VendorId { get; set; }
+        public string Document { get; set; }
+        public int  Score { get; set; }
+    }
+}
