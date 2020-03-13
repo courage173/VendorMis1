74fc67ad5f55905e07a0b9ffcd20490581f85df8
courage173 <courageosemwengie@gmail.com>
2020-03-13 10:32:25 +0100

    exclude config file

--- VendorMis/App_Start/BundleConfig.cs
+++ VendorMis/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace VendorMis
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                         "~/Content/assets/scripts/main.js"
+                         //"~/Scripts/jquery-{version}.js"
+                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,11 +23,16 @@ namespace VendorMis
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+                      "~/Content/main.css"
+
+                      //"~/Content/bootstrap.css",
+                      //"~/Content/site.css"
+                      ));
         }
     }
 }
