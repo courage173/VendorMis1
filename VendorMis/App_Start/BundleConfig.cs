faf80b12f4c51ba0e4d0b150445277a49f55dabe
Benjamin Daziba Inemugha <bennyblasco@gmail.com>
2020-03-17 12:04:24 +0100

    Put Authorization in Home Controller

--- VendorMis/App_Start/BundleConfig.cs
+++ VendorMis/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace VendorMis
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"
+                      "~/Script/bootstrap.js"/*,*/
+                      //"~/Scripts/respond.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
